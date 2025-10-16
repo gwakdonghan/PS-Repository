5faa7e67ca80df0328135a85b87db083e5fd6f60
gwakdonghan <gwakdonghan92@gmail.com>
2025-10-16 22:23:23 +0800

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,7 +1,4 @@
 def solution(a, b):
-    # a ⊕ b 계산
-    ab = int(str(a) + str(b))
-    # b ⊕ a 계산
-    ba = int(str(b) + str(a))
-    # 더 큰 값을 반환
-    return max(ab, ba)
+    ab = int(str(a) + str(b))  # a ⊕ b
+    ba = int(str(b) + str(a))  # b ⊕ a
+    return max(ab, ba)  # 더 큰 숫자 반환
